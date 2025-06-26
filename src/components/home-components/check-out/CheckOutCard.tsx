98dce29e0db165a05901658c5bd80f6d472338a5
mrravan0 <ravanndzf@gmail.com>
2025-06-26 23:53:44 +0400

    Added CheckOutCard Component

---
+++ src/components/home-components/check-out/CheckOutCard.tsx
@@ -0,0 +1,66 @@
+import type { FC } from "react";
+import HeartIcon from "../../../svg/CheckOutIcons/HeartIcon";
+import CommentIcon from "../../../svg/CheckOutIcons/CommentIcon";
+import SaveIcon from "../../../svg/CheckOutIcons/SaveIcon";
+import type { HomeCheckOutCardProps } from "../../../fake-data/home-data/HomeTypes";
+import MessengerIcon from "../../../svg/CheckOutIcons/MessengerIcon";
+
+const CheckOutCard: FC<HomeCheckOutCardProps> = ({ data }) => {
+  return (
+    <div className="flex cursor-pointer flex-col gap-y-2.5 rounded-lg bg-white py-2.5 duration-300 hover:scale-95">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2.5 pr-3 pl-2.5">
+          <img
+            src="/home-page/homeCheckOut/photo1.png"
+            alt=""
+          />
+          <div className="flex flex-col gap-y-1">
+            <h2 className="font-semibold">Foodieland</h2>
+            <p className="text-sm font-light">Tokyo,Japan</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-x-0.5">
+          <span className="size-0.5 rounded-full bg-black"></span>
+          <span className="size-0.5 rounded-full bg-black"></span>
+          <span className="size-0.5 rounded-full bg-black"></span>
+        </div>
+      </div>
+      <img
+        src={data.image}
+        alt=""
+      />
+      <div className="flex flex-col gap-y-2.5 pr-3 pl-2.5">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-x-3.5">
+            <HeartIcon />
+            <CommentIcon />
+            <MessengerIcon />
+          </div>
+          <SaveIcon />
+        </div>
+        <div className="flex flex-col gap-y-1">
+          <div className="flex items-center gap-x-1.25">
+            <img
+              src="/home-page/homeCheckOut/photo2.png"
+              alt=""
+            />
+            <p>
+              Liked by
+              <span className="font-bold"> {data.title}</span>
+              and
+              <span className="font-bold">44,686</span>
+              <br /> others
+            </p>
+          </div>
+          <p>
+            <span className="font-semibold">Foodieland.</span>
+            {data.description}
+          </p>
+        </div>
+        <p className="text-custom-grey-40">September,19</p>
+      </div>
+    </div>
+  );
+};
+
+export default CheckOutCard;
